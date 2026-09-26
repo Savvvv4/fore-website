30e65a470d1ab951e000c57c52b8cf5ea6547849
Savdeep Kadian <2317140@kmc.du.ac.in>
2026-09-26 01:53:15 -0700

    Fix search and AI crawler discoverability

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,45 @@
 import type { MetadataRoute } from 'next';
 
+const sitemap = 'https://www.foresports.in/sitemap.xml';
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://foresports.in/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+      },
+      {
+        userAgent: 'OAI-SearchBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      },
+    ],
+    sitemap,
+    host: 'https://www.foresports.in',
   };
 }
